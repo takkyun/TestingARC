12ddbcb12f8e2023f258c3d9d28352d50e34b676
takkyun <takuya.otani@gmail.com>
2012-02-20 14:36:45 +1300

    Initial Commit

---
+++ TestingARC/SBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBAppDelegate.h
+//  TestingARC
+//
+//  Created by Takuya Otani on 20/02/12.
+//  Copyright (c) 2012 SerendipityNZ Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
